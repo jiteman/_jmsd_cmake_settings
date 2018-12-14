f8a74be37fd932188a004317c368fd1bc193e289
Jiteman <jiteman@gmail.com>
2018-12-14 19:22:01 +0300

    changes for dependency string/lists (incomplete)

--- cmake/macroses/macroses.cmake
+++ cmake/macroses/macroses.cmake
@@ -1,57 +1,8 @@
 macro(
-	JMSD_REMOVE_PLATFORM_HEADER_FILES_FOR_POCO
-	file_list
-	file_list_to_remove
-	subfolder
+	JMSD_LIST_TO_STRING
+	in_list_out_string		
 )
-	set( JMSD_PLATFORM_LIST "Android;C99;DEC;DUMMY;HPUX;POSIX;QNX;STD;STD_POSIX;STD_VX;STD_WIN32;SUN;UNIX;VMS;VX;WIN32;WIN32_OSVER;WIN32U;WINCE" )
-
-	foreach( platform_name ${JMSD_PLATFORM_LIST} )
-		foreach( filename ${${file_list_to_remove}} )
-			list( REMOVE_ITEM ${file_list} ${PROJECT_SOURCE_DIR}/${subfolder}/${filename}_${platform_name}.h )
-		endforeach()
-	endforeach()
-
-endmacro()
-
-macro(
-	JMSD_REMOVE_PLATFORM_CXX_SOURCE_FILES_FOR_POCO
-	file_list
-	file_list_to_remove
-	subfolder
-)
-	set( JMSD_PLATFORM_LIST "Android;C99;DEC;DUMMY;HPUX;POSIX;QNX;STD;STD_POSIX;STD_VX;STD_WIN32;SUN;UNIX;VMS;VX;WIN32;WIN32_OSVER;WIN32U;WINCE" )
-
-	foreach( platform_name ${JMSD_PLATFORM_LIST} )
-		foreach( filename ${${file_list_to_remove}} )
-			list( REMOVE_ITEM ${file_list} ${PROJECT_SOURCE_DIR}/${subfolder}/${filename}_${platform_name}.cpp )
-		endforeach()
-	endforeach()
-
-endmacro()
-
-macro(
-	JMSD_REMOVE_HEADER_FILES_FOR_POCO
-	file_list
-	file_list_to_remove
-	subfolder
-)
-	foreach( filename ${${file_list_to_remove}} )
-		list( REMOVE_ITEM ${file_list} ${PROJECT_SOURCE_DIR}/${subfolder}/${filename}.h )
-	endforeach()
-
-endmacro()
-
-macro(
-	JMSD_REMOVE_CXX_SOURCE_FILES_FOR_POCO
-	file_list
-	file_list_to_remove
-	subfolder
-)
-	foreach( filename ${${file_list_to_remove}} )
-		list( REMOVE_ITEM ${file_list} ${PROJECT_SOURCE_DIR}/${subfolder}/${filename}.cpp )
-	endforeach()
-
+	string( REPLACE ";" " " ${in_list_out_string} "${${in_list_out_string}}" )
 endmacro()
 
 if ( UNIX )
