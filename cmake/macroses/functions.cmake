9cd3ea782ecca1d022173e6a60178d1c7e0898ec
jiteman <jiteman@gmail.com>
2019-01-15 18:47:18 +0000

    remove file(s) from the list functions has been added

--- cmake/macroses/functions.cmake
+++ cmake/macroses/functions.cmake
@@ -20,36 +20,53 @@ endfunction()
 
 function(
 	JMSD_REMOVE_FILE_FROM_THE_LIST
-	out_list
-	in_list
+	out_in_file_list
 	file_name_to_remove
 )
-	set( found_file_indices "" )
+	set( found_file_indicies "" )
+	set( filename_index 0 )
 
-	foreach( file_path ${in_list} )
+	foreach( file_path ${${out_in_file_list}} )
 		string( FIND ${file_path} ${file_name_to_remove} found_index )
 
 		if( NOT found_index EQUAL -1 )
-			list( APPEND found_file_indicies ${found_index} )
+			list( APPEND found_file_indicies ${filename_index} )
 		endif()
+
+		math( EXPR filename_index "${filename_index} + 1" )
 	endforeach()
 
-	list( REVERSE found_file_indicies )
+	list( LENGTH found_file_indicies index_quantity )
 
-	foreach( file_index ${found_file_indicies} )
-		list( REMOVE_AT ${${in_out_list}} ${file_index} )
-	endforeach()
+	if( ${index_quantity} GREATER 0 )
+		if( ${index_quantity} GREATER 1 )
+			list( REVERSE found_file_indicies )
+		endif()
+
+		set( out_list ${${out_in_file_list}} )
+
+		foreach( file_index ${found_file_indicies} )
+			list( REMOVE_AT out_list ${file_index} )
+		endforeach()
+	endif()
+
+	set( ${out_in_file_list} ${out_list} PARENT_SCOPE )
 
 endfunction()
 
 
 function(
 	JMSD_REMOVE_FILES_FROM_THE_LIST
-	in_out_list
+	out_in_file_list
 	file_name_to_remove_list
 )
+	set( out_in_list ${${out_in_file_list}} )
+
+
 	foreach( filename ${${file_name_to_remove_list}} )
-		JMSD_REMOVE_FILE_FROM_THE_LIST( ${in_out_list} ${filename} )
+		JMSD_REMOVE_FILE_FROM_THE_LIST( out_in_list ${filename} )
 	endforeach()
+
+	set( ${out_in_file_list} ${out_in_list} PARENT_SCOPE )
 
 endfunction()
