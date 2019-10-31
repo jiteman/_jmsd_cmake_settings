f8f04c3c2752ca3592d03f345d45fe6dd40ca978
jiteman <jiteman@gmail.com>
2019-10-31 17:11:14 +0300

    file rename has been done

--- cmake/utilities/functions-and-macroses.cmake
+++ cmake/utilities/functions-and-macroses.cmake
@@ -1,3 +1,3 @@
 include( ${JMSD_CMAKE_SETTINGS_PATH}/utilities/macroses.cmake )
 include( ${JMSD_CMAKE_SETTINGS_PATH}/utilities/functions.cmake )
-include( ${JMSD_CMAKE_SETTINGS_PATH}/utilities/show_functions.cmake )
+include( ${JMSD_CMAKE_SETTINGS_PATH}/utilities/show-functions.cmake )
